d6ff8543a172dde308443b1207584d482309ba82
CoachGdub <vball42@gmail.com>
2023-02-15 12:11:03 -0500

    Hopefully pushing main?

---
+++ main/main.js
@@ -0,0 +1,28 @@
+
+function myFunction() {
+    var x = document.getElementById("searchInput").elements[0].value;
+    if (x == "")
+        getContacts();
+    else
+        document.getElementById("results").innerHTML = "Prompt search for: " + x;
+  }
+
+function getContacts(userId) {
+    console.log("getting all contacts...");
+    var obj, xmlhttp, myObj, x, txt = "";
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // myObj = JSON.parse(this.responseText);
+            // for (x in myObj) {
+            //     txt += myObj[x].names + "<br>";
+            // }
+            // document.getElementById("arrayContent").innerHTML = txt;
+            console.log(`Response = ${JSON.parse(this.responseText)}`);
+            document.getElementById("test-all").innerHTML =
+                          this.responseText;
+        }
+    };
+    xmlhttp.open("GET", "https://codegojolt.xyz/LAMPAPI/getContacts.php?ID=20", true);
+    xmlhttp.send();
+}
