257b08d32b08b856ed3d1da2ee7f1656ff914e61
CoachGdub <vball42@gmail.com>
2023-02-18 16:04:15 -0500

    Got modal buttons working in last column of appended records

--- home/main.js
+++ home/main.js
@@ -24,19 +24,31 @@ function getContacts(userId) {
 }
 
 function fillResponse(myObj) {
+    // Get beginning of table body
     var displayTable = document.getElementById('output-table-body');
+    // Loop for each row in response
     for (let i=0; i < myObj.length; i++) {
         var rec = [myObj[i].FirstName, myObj[i].LastName, myObj[i].Email, myObj[i].Phone];
-        let newRow = document.createElement("tr");
+        // Begin new row
+        let newCell, newRow = document.createElement("tr");
+        // Loop for each column in record
         for (let j=0; j < rec.length; j++) {
-            let newCell = document.createElement("td");
+            newCell = document.createElement("td");
             newCell.innerHTML = rec[j];
             newRow.appendChild(newCell);
         }
+        // Add modal to last column
+        newCell = document.createElement("td");
+        let modalButton = document.createElement("button");
+        modalButton.type = "button";
+        modalButton.setAttribute("class", "btn btn-link");
+        modalButton.setAttribute("data-bs-toggle", "modal");
+        modalButton.setAttribute("data-bs-target", "#aboutModal");
+        modalButton.innerHTML = "Manage";
+        newCell.appendChild(modalButton);
+        newRow.appendChild(newCell);
+
+        // Add completed record to the DOM
         displayTable.appendChild(newRow);
-        // document.getElementById("arrayContent").innerHTML = txt;
-        // console.log(`Response = ${myObj}`);
-        // document.getElementById("test-all").innerHTML =
-        //           JSON.stringify(myObj);
     }
 }
