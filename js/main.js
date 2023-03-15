51c3cb240a93357098f0bc835dd7c74baa1f6cf7
CoachGdub <vball42@gmail.com>
2023-03-14 20:45:24 -0400

    1) added new contact button to main.html (TBD - fix layout positioning) 2) added onclick commands (delete and save) to modal in main.html 3) added connectivity of modal data to table data in main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,59 @@
 "use strict";
 
+const manageModal = document.getElementById('aboutModal');
+// Function to update modal contents with corresponding record data
+manageModal.addEventListener('show.bs.modal', event =>
+    {
+        // Button that triggered the modal
+        const button = event.relatedTarget;
+
+        // Check if editing existing contact (Manage button)
+        if (button.innerHTML == "Manage") {
+            // Extract info from row fields
+            const firstName = button.parentElement.parentElement.childNodes[0].innerHTML;
+            const lastName = button.parentElement.parentElement.childNodes[1].innerHTML;
+            const email = button.parentElement.parentElement.childNodes[2].innerHTML;
+            const phone = button.parentElement.parentElement.childNodes[3].innerHTML;
+            // Update the modal's content.
+            manageModal.querySelector('#firstInput').value = firstName;
+            manageModal.querySelector('#lastInput').value = lastName;
+            manageModal.querySelector('#emailInput').value = email;
+            manageModal.querySelector('#phoneInput').value = phone;
+        }
+        // Otherwise we opened modal with add new contact button
+        else {
+            // Clear the modal's content.
+            manageModal.querySelector('#firstInput').value = "";
+            manageModal.querySelector('#lastInput').value = "";
+            manageModal.querySelector('#emailInput').value = "";
+            manageModal.querySelector('#phoneInput').value = "";
+            // Update and hide button text for relevance
+            //TBD
+        }
+    }
+)
+
+function modalAddNew(userID) {
+
+}
+
+function modalUpdate(userID) {
+
+}
+
+function modalDelete(userID) {
+
+}
+
 function myFunction(userID) {
     var x = document.getElementById("searchInput").elements[0].value;
     var url = "https://codegojolt.xyz/LAMPAPI/";
     getContacts(userID, url, x);
 }
 
-function getContacts(userId, url, search) {
+function getContacts(userID, url, search) {
     console.log("getting all contacts...");
-    var xmlhttp, myObj, payload = {"ID": userId};
+    var xmlhttp, myObj, payload = {"ID": userID};
     // Search box empty
     if (search == "")
         url += "getContacts.php";
@@ -19,8 +64,8 @@ function getContacts(userId, url, search) {
     // document.getElementById("results").innerHTML = "Prompt search for: " + x;
 }
     // Debug
-    console.log(`url = ${url}`);
-    console.log(`JSON = ${JSON.stringify(payload)}`);
+    // console.log(`url = ${url}`);
+    // console.log(`JSON = ${JSON.stringify(payload)}`);
 
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
@@ -39,7 +84,7 @@ function clearResults() {
     // Get beginning of table body
     var displayTable = document.getElementById('output-table-body');
     // Loop through and remove all children
-    while (displayTable.lastElementChild) {
+    while (displayTable.lastElementChild && displayTable.lastElementChild != document.getElementById("addButtonRow")) {
         displayTable.removeChild(displayTable.lastElementChild);
     }
 }
@@ -52,6 +97,12 @@ function fillResponse(myObj) {
         var rec = [myObj[i].ID, myObj[i].FirstName, myObj[i].LastName, myObj[i].Email, myObj[i].Phone];
         // Begin new row
         let newCell, newRow = document.createElement("tr");
+        let rowID = "contactNum" + rec[0].toString();
+        newRow.setAttribute("id", rowID);
+        // Alternate light shading
+        if (i % 2 == 1) {
+            newRow.setAttribute("class", "table-secondary");
+        }
         // Loop for each column in record
         for (let j=1; j < rec.length; j++) {
             newCell = document.createElement("td");
