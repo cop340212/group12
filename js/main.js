50f76371669eef14b07d7dab8e0d65de4bb82f4f
CoachGdub <vball42@gmail.com>
2023-03-15 12:31:19 -0400

    Implemented add new user functionality (successfully adding a user switches modal to update mode)

--- js/main.js
+++ js/main.js
@@ -6,9 +6,12 @@ manageModal.addEventListener('show.bs.modal', event =>
     {
         // Button that triggered the modal
         const button = event.relatedTarget;
+        let saveButton = document.getElementById("saveUpdate");
 
         // Check if editing existing contact (Manage button)
-        if (button.innerHTML == "Manage") {
+        if (button.classList.contains("myManageModalButton")) {
+            // Make sure Delete button is showing
+            document.getElementById("delete").classList.remove("visually-hidden");
             // Extract info from row fields
             const firstName = button.parentElement.parentElement.childNodes[0].innerHTML;
             const lastName = button.parentElement.parentElement.childNodes[1].innerHTML;
@@ -19,6 +22,8 @@ manageModal.addEventListener('show.bs.modal', event =>
             manageModal.querySelector('#lastInput').value = lastName;
             manageModal.querySelector('#emailInput').value = email;
             manageModal.querySelector('#phoneInput').value = phone;
+            // Use more appropriate action word
+            saveButton.innerHTML = "Save changes";
         }
         // Otherwise we opened modal with add new contact button
         else {
@@ -28,17 +33,78 @@ manageModal.addEventListener('show.bs.modal', event =>
             manageModal.querySelector('#emailInput').value = "";
             manageModal.querySelector('#phoneInput').value = "";
             // Update and hide button text for relevance
-            //TBD
+            saveButton.innerHTML = "Save";
+            document.getElementById("delete").classList.add("visually-hidden");
         }
     }
 )
 
 function modalAddNew(userID) {
+    // Grab data fields from modal
+    const firstName = manageModal.querySelector('#firstInput').value;
+    const lastName = manageModal.querySelector('#lastInput').value;
+    const email = manageModal.querySelector('#emailInput').value;
+    const phone = manageModal.querySelector('#phoneInput').value;
 
+    // Build POST request
+    var url = "https://codegojolt.xyz/LAMPAPI/createContact.php";
+    console.log("adding new contact...");
+    var xmlhttp, newID, myObj = [];
+    var payload = {"UserID": userID, "FirstName": firstName, "LastName": lastName, "Email": email, "Phone": phone};
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function()
+    {
+        if (this.readyState == 4) 
+        {
+            switch(this.status)
+            {
+                // OK
+                case 200:
+                    newID = JSON.parse(this.responseText).ID;
+                    console.log(newID);
+                    // Update modal with success message and switch to update mode
+                    document.getElementById("delete").classList.remove("visually-hidden");
+                    document.getElementById("saveUpdate").saveButton.innerHTML = "Save changes";
+                    // Swap Contact ID for UserID and add new record to displayed results table
+                    delete payload["UserID"];
+                    payload["ID"] = newID;
+                    myObj.push(payload);
+                    let isLastShaded = document.getElementById('output-table-body').lastChild.classList.contains("table-light");
+                    fillResponse(myObj, !isLastShaded);
+                    break;
+                // Contact already exists
+                case 401:
+                    console.log("Error 401: Contact already exists");
+                    break;
+                // Could Not Connect to Database
+                case 403:
+                    console.log("Error 401: Could Not Connect to Database");
+                    break;
+                // URL Not Found
+                case 404:
+                    console.log("Error 401: URL Not Found");
+                    break;
+                // \o/
+                default:
+                    console.log("What did you do to get here?!")
+            }
+        }
+    };
+    xmlhttp.open("POST", url, true);
+    xmlhttp.send(JSON.stringify(payload));
 }
 
 function modalUpdate(userID) {
+    if (document.getElementById("saveUpdate").innerHTML = "Save")
+    // Modal reached by clicking "New Contact" button
+    {
+        modalAddNew(userID);
+    }
+    else
+    // Modal reached by clicking "Manage" button in a contact record
+    {
 
+    }
 }
 
 function modalDelete(userID) {
@@ -73,7 +139,7 @@ function getContacts(userID, url, search) {
             console.log("About to parse and assign to myObj")
             myObj = JSON.parse(this.responseText);
             clearResults();
-            fillResponse(myObj);
+            fillResponse(myObj, 0);
          }
     };
     xmlhttp.open("POST", url, true);
@@ -89,7 +155,7 @@ function clearResults() {
     }
 }
 
-function fillResponse(myObj) {
+function fillResponse(myObj, makeFirstShaded) {
     // Get beginning of table body
     var displayTable = document.getElementById('output-table-body');
     // Loop for each row in response
@@ -100,8 +166,8 @@ function fillResponse(myObj) {
         let rowID = "contactNum" + rec[0].toString();
         newRow.setAttribute("id", rowID);
         // Alternate light shading
-        if (i % 2 == 1) {
-            newRow.setAttribute("class", "table-secondary");
+        if ((i + makeFirstShaded) % 2 == 1) {
+            newRow.setAttribute("class", "table-light");
         }
         // Loop for each column in record
         for (let j=1; j < rec.length; j++) {
@@ -113,7 +179,7 @@ function fillResponse(myObj) {
         newCell = document.createElement("td");
         let modalButton = document.createElement("button");
         modalButton.type = "button";
-        modalButton.setAttribute("class", "btn btn-link");
+        modalButton.setAttribute("class", "btn btn-link myManageModalButton");
         modalButton.setAttribute("data-bs-toggle", "modal");
         modalButton.setAttribute("data-bs-target", "#aboutModal");
         modalButton.innerHTML = "Manage";
